b7969a1481119887079f06aa61d9144da65dfe2c
durzoflint <abhinavu1201@gmail.com>
2018-04-30 16:50:28 +0530

    Basic UI

--- app/src/main/java/durzoflint/celebritywallpaper/HomeActivity.java
+++ app/src/main/java/durzoflint/celebritywallpaper/HomeActivity.java
@@ -2,6 +2,14 @@ package durzoflint.celebritywallpaper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ir.apend.slider.model.Slide;
+import ir.apend.slider.ui.Slider;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +18,25 @@ public class HomeActivity extends AppCompatActivity {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Slider slider = findViewById(R.id.slider);
+
+        //create list of slides
+        List<Slide> slideList = new ArrayList<>();
+        slideList.add(new Slide(0,"http://cssslider.com/sliders/demo-20/data1/images/picjumbo.com_img_4635.jpg" , 0/*getResources().getDimensionPixelSize(R.dimen.slider_image_corner)*/));
+        slideList.add(new Slide(1,"http://cssslider.com/sliders/demo-12/data1/images/picjumbo.com_hnck1995.jpg" , 0));
+        slideList.add(new Slide(2,"http://cssslider.com/sliders/demo-19/data1/images/picjumbo.com_hnck1588.jpg" , 0));
+        slideList.add(new Slide(3,"http://wowslider.com/sliders/demo-18/data1/images/shanghai.jpg" , 0));
+
+        //handle slider click listener
+        slider.setItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //do what you want
+            }
+        });
+
+        //add slides to slider
+        slider.addSlides(slideList);
     }
 }
