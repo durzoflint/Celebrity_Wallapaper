8d68c2b219bcd0e49273e41f5a04487b4d497f97
durzoflint <abhinavu1201@gmail.com>
2018-05-06 04:50:59 +0530

    Set Wallpaper

--- app/src/main/java/durzoflint/celebritywallpaper/FullScreenActivity.java
+++ app/src/main/java/durzoflint/celebritywallpaper/FullScreenActivity.java
@@ -1,12 +1,20 @@
 package durzoflint.celebritywallpaper;
 
+import android.app.WallpaperManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
 
 public class FullScreenActivity extends AppCompatActivity {
 
@@ -15,7 +23,7 @@ public class FullScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_screen);
 
-        ImageView imgFull=findViewById(R.id.imgFull);
+        final ImageView imgFull=findViewById(R.id.imgFull);
 
         //supportPostponeEnterTransition();
 
@@ -35,5 +43,22 @@ public class FullScreenActivity extends AppCompatActivity {
                         supportStartPostponedEnterTransition();
                     }
                 });
+
+        Button  setAsWallpaper = findViewById(R.id.setaswallpaper);
+        setAsWallpaper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WallpaperManager wallpaperManager = WallpaperManager.getInstance(FullScreenActivity.this);
+                BitmapDrawable bitmapDrawable = (BitmapDrawable) imgFull.getDrawable();
+                Bitmap bitmap = bitmapDrawable.getBitmap();
+                try {
+                    wallpaperManager.setBitmap(bitmap);
+                    Toast.makeText(FullScreenActivity.this, "Wallpaper set successfully", Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(FullScreenActivity.this, "Error while setting wallpaper", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
