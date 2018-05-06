fd2e97e80ed305ae8cbfa82f04c40476e2c68b09
durzoflint <abhinavu1201@gmail.com>
2018-05-06 13:05:05 +0530

    Menu

--- app/src/main/java/durzoflint/celebritywallpaper/HomeActivity.java
+++ app/src/main/java/durzoflint/celebritywallpaper/HomeActivity.java
@@ -3,6 +3,8 @@ package durzoflint.celebritywallpaper;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
@@ -138,4 +140,31 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         intent.putExtra("name", name);
         startActivity(intent);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.contribute:
+                break;
+            case R.id.donate:
+                break;
+            case R.id.report:
+                break;
+            case R.id.about:
+                break;
+            case R.id.contact:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
